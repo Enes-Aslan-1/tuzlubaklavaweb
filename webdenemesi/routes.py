a1571667faac467176e72f6668075ef8e2b28f13
Enes-Aslan-1 <enesaslanenesaslan1@gmail.com>
2021-03-06 12:52:53 +0300

    first commit

---
+++ webdenemesi/routes.py
@@ -0,0 +1,11 @@
+from webdennemesi import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template("index.html")
+
+@app.route('/commands')
+def commands():
+    return render_template("commands.html", title = "Komutlar")
